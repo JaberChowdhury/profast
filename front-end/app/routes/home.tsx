d25c6c438b7cc8dd7a49804c4c403e940e5f0f34
black <jaberhc2002@gmail.com>
2025-07-02 17:34:28 +0600

    feat: enhance BlurImage and HorizontalSeparator components; update Invitation layout for improved design and structure

--- front-end/app/routes/home.tsx
+++ front-end/app/routes/home.tsx
@@ -4,7 +4,6 @@ import Hero from "~/components/custom/Hero";
 import Howitworks from "~/components/custom/How_it_works";
 import Our_services from "~/components/custom/Our_services";
 import Our_servicesv2 from "~/components/custom/Our_servicesv2";
-import { useRef } from "react";
 import HorzintalSeparator from "~/components/custom/HorzintalSeparator";
 import Invitation from "~/components/custom/Invitation";
 
@@ -19,14 +18,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-28">
       <Hero />
-      <div className="md:my-24">
-        <Howitworks />
-      </div>
+      <Howitworks />
       <HorzintalSeparator />
       <Our_services />
       <Brands />
       <Our_servicesv2 />
-      <HorzintalSeparator  />
+      <HorzintalSeparator />
       <Invitation />
     </div>
   );
